8892fb17599993ef7481e3aa59ea10eee4b95c8a
Łukasz 'Cyber Killer' Korpalski <cyberkiller8@gmail.com>
2024-04-03 20:52:03 +0200

    argument parser

--- puzzgal.py
+++ puzzgal.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 
+"""
+PuzzGal - a static jigsaw puzzle gallery creator
+Copyright (C) 2024 Łukasz 'CyberKiller' Korpalski
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import argparse
+
 class PuzzGal:
   def __init__(self, path):
     self.path = path
+    print(self.path)
 
   def read_config(self):
     raise NotImplementedError()
@@ -14,5 +35,8 @@ class PuzzGal:
     raise NotImplementedError()
 
 if __name__ == "__main__":
-  path = ""
-  app = PuzzGal(path)
+  parser = argparse.ArgumentParser(prog='PuzzGal',description='a static jigsaw puzzle gallery creator')
+  parser.add_argument('path', nargs=1, help='path to a directory with puzzle photos')
+  args = parser.parse_args()
+
+  app = PuzzGal(args.path[0])
