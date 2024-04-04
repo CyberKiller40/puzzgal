201273b2ca46e994de4c4fd6c772cdcdfd3d4e77
Łukasz 'Cyber Killer' Korpalski <cyberkiller8@gmail.com>
2024-04-04 22:16:36 +0200

    minor correction

--- puzzgal.py
+++ puzzgal.py
@@ -42,25 +42,27 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='PuzzGal - a static jigsaw puzzle gallery creator', epilog='Project website: https://github.com/CyberKiller40/puzzgal')
   parser.add_argument('path', nargs=1, type=pathlib.Path, help='path to a directory with puzzle photos')
   parser.add_argument('-v', '--version', action='version', version=app_version)
-  parser.add_argument('-l', '--loglevel', dest='loglevel', choices=['INFO', 'WARNING', 'ERROR'], default='INFO', help='logging level')
+  parser.add_argument('-l', '--loglevel', dest='loglevel', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], default='INFO', help='logging level, defaults to INFO')
   parser.add_argument('--no-logfile', dest='nologfile', action='store_true', help='set to ommit creating a log file and output messages to the terminal')
   args = parser.parse_args()
 
   logformat='%(asctime)s %(levelname)s: %(message)s'
-  if args.nologfile == False:
+  if args.nologfile == True:
+    logging.basicConfig(format=logformat)
+  else:
     try:
       logging.basicConfig(filename=args.path[0]/'puzzgal.log', format=logformat)
     except FileNotFoundError:
       logging.basicConfig(format=logformat)
       logging.warning("Path invalid, logging to terminal.")
-  else:
-    logging.basicConfig(format=logformat)
   if args.loglevel == 'ERROR':
     logger.setLevel(logging.ERROR)
   elif args.loglevel == 'WARNING':
     logger.setLevel(logging.WARNING)
   elif args.loglevel == 'INFO':
     logger.setLevel(logging.INFO)
+  elif args.loglevel == 'DEBUG':
+    logger.setLevel(logging.DEBUG)
   else:
     logger.setLevel(logging.INFO)
 
